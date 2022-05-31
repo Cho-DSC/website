aa4d61a5633cf0970eae5a98a14a782054742922
JunHyeong Cho <jojunhy@gmail.com>
2022-05-31 17:17:48 +0900

    [Update] router-404 page

--- src/router.js
+++ src/router.js
@@ -1,16 +1,21 @@
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import NotFound from './views/NotFound.vue'
 import { createWebHistory, createRouter } from "vue-router"
 import Vue from 'vue'
 
 const routes = [
     {
-    path: '/',
-    component: Home
+        path: '/',
+        component: Home
     },
     {
-    path: '/about',
-    component: About
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/:catchAll(.*)',
+        component: NotFound
     },
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>404 Not Found.<br>
+    Oops, it looks like the page you're looking for doesn't exist.</h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>>
